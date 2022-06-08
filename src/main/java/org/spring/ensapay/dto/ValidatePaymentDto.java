880be3644fad10e66e30f42bbe68799142a130a6
unknown <zaka.raja.1949@gmail.com>
2022-06-08 19:32:34 +0100

    validation added

--- src/main/java/org/spring/ensapay/dto/ValidatePaymentDto.java
+++ src/main/java/org/spring/ensapay/dto/ValidatePaymentDto.java
@@ -5,16 +5,31 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 @Setter
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 public class ValidatePaymentDto {
 
+    @NotBlank(message = "Token is required")
+    @Pattern(regexp = "^[0-9]+[0-9]$")
+    @Size(min = 6,max = 6)
     private Integer generatedToken;
+    @NotBlank(message = "Your Id is required")
+    @Pattern(regexp = "^[0-9]+[0-9]$")
     private Long clientId;
+    @NotBlank(message = "Your Facture impaye is required")
+    @Pattern(regexp = "^[0-9]+[0-9]$")
     private Integer impaye;
+    @NotBlank(message = "Code Credetor is required")
+    @Pattern(regexp = "^[a-zA-Z1-9]+$",message = "should only have letter and numbers")
     private String codeCreditor;
+    @NotBlank(message = "Code Debt is required")
+    @Pattern(regexp = "^[1-9]+$",message = "should only have numbers")
     private String codeDept;
 
 }
